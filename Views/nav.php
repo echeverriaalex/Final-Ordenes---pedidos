f32b1bce5675f1e4c080163d5b9f5ba284c8790a
echeverriaalex <aleex.naahuel@outlook.com>
2023-02-12 12:16:48 -0300

    agrego vista para ver todas las ordenes

--- Views/nav.php
+++ Views/nav.php
@@ -7,8 +7,11 @@
                <a class="nav-link" href="<?php echo FRONT_ROOT ?>Order/ShowAddView">Agregar Ordenes</a>
           </li>
           <li class="nav-item">
-               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Order/ShowListView">Listar Ordenes</a>
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Order/ShowListOrderPendingView">Listar ordenes pendientes</a>
           </li>   
+          <li class="nav-item">
+               <a class="nav-link" href="<?php echo FRONT_ROOT ?>Order/ShowListAllOrderView">Listar todas las ordenes</a>
+          </li> 
           <li class="nav-item">
                <a class="nav-link" href="<?php echo FRONT_ROOT ?>User/Logout">Cerrar Sesión</a>
           </li>        
